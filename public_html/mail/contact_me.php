f6147d3badcad4a0986bf8e836e3160157b1f99a
Teo Gonzalez <teo.gonzalez.c@hotmail.com>
2020-01-03 01:06:28 -0700

    Cleaning up before sarting fresh

--- public_html/mail/contact_me.php
+++ public_html/mail/contact_me.php
@@ -42,7 +42,7 @@ $phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
 	
 // Create the email and send the message
-$to = 'teo.gonzalez.c@hotmail.com';
+$to = 'contact@thblckjkr.tk';
 $email_subject = "Website Contact Form:  $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
 $headers = "From: noreply@thblckjkr.000webhostapp.com\n";
